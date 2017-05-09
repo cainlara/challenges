0118822ea82f56c21a88615ca06bae2ba8c47309
Jose Andrés Lara <jose.andreas.lara@one.verizon.com>
2017-05-09 10:47:27 -0700

    Adding Street Fighter.

---
+++ src/com/challenges/test/CharacterSelection2Test.java
@@ -0,0 +1,14 @@
+package com.challenges.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CharacterSelection2Test {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
