ef7bd733ce99912c8eeef3158959bb05b8a3cf9d
Jose Andrés Lara <jose.andreas.lara@one.verizon.com>
2017-07-12 14:47:47 -0700

    Adding Recursive Fusc challeng.

---
+++ src/com/challenges/test/FuscTest.java
@@ -0,0 +1,18 @@
+package com.challenges.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.challenges.Fusc;
+
+public class FuscTest {
+
+  @Test
+  public void tests() {
+    assertEquals(0, Fusc.fusc(0));
+    assertEquals(1, Fusc.fusc(1));
+    assertEquals(21, Fusc.fusc(85));
+  }
+
+}
